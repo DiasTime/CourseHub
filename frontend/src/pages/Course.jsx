305d7dccec837f11a2005fdda9c5ece1d4ea18ff
sapargalievvv <sapargalievbektas071@gmail.com>
2024-03-23 16:08:55 +0600

    ya umer

--- frontend/src/pages/Course.jsx
+++ frontend/src/pages/Course.jsx
@@ -6,12 +6,24 @@ import { db } from '../firebase';
 import { onSnapshot, collection } from 'firebase/firestore';
 import { setCourses } from '../redux/slices/courseSlice';
 import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Course = () => {
   const [foundCourse, setFoundCourse] = useState([]);
   const { courses } = useSelector(courseSelector);
   const dispatch = useDispatch();
   const { courseId } = useParams();
+
+  const [isSidebarVisible, setIsSidebarVisible] = useState(false);
+  const [selectedChapter, setSelectChapter] = useState('');
+
+  const handleMouseEnter = () => {
+    setIsSidebarVisible(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsSidebarVisible(false);
+  };
 
   useEffect(() => {
     const collectionRef = collection(db, 'courses');
@@ -36,9 +48,33 @@ const Course = () => {
     }
   }, [courses]);
 
-  console.log(foundCourse);
+  const [currentCourse] = foundCourse;
 
-  return <div className="wrapper">{courseId}</div>;
+  if (currentCourse) {
+    console.log(currentCourse.chapters);
+    var filteredChapter = currentCourse.chapters.filter((item) => item.title == selectedChapter);
+  }
+
+  return (
+    <div className="courseLession">
+      <div className="sidebar" onMouseLeave={handleMouseLeave}>
+        <div className={`sideMenu ${isSidebarVisible ? 'sideMenu-visible' : ''}`}>
+          {currentCourse &&
+            currentCourse.chapters &&
+            currentCourse.chapters.map((item) => (
+              <li key={item.title} onClick={() => setSelectChapter(item.title)}>
+                {item.title}
+              </li>
+            ))}
+        </div>
+        <div className="sideHint" onMouseEnter={handleMouseEnter}></div>
+      </div>
+
+      <div className="wrapper">
+        <h1>{filteredChapter && filteredChapter[0].content}</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Course;
